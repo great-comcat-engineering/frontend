cef0b9b015a9f766cca6f99ffe1c24281ee774b5
gowth6m <gowtham4n@gmail.com>
2024-03-24 18:58:50 +0000

    Add base nav bar component

--- src/components/navigation/nav-desktop.tsx
+++ src/components/navigation/nav-desktop.tsx
@@ -9,53 +9,60 @@ import {
 } from "../ui/navigation-menu";
 import { NavListItem } from "./nav-menu-list-item";
 import { navMenuItems } from "./nav-menu-items";
+import GCELogo from "../core/gce-logo";
 
 const NavDesktop: React.FC = () => {
     return (
-        <NavigationMenu className="md:flex hidden container mx-auto">
-            <NavigationMenuList>
-                {navMenuItems.map((item, index) => {
-                    if (item.subItems) {
+        <div className="fixed top-0 left-1/2 -translate-x-1/2">
+            <div className="flex bg-slate-800 w-screen h-16">
+                <GCELogo />
+            </div>
+
+            <NavigationMenu className="md:flex hidden container mx-auto">
+                <NavigationMenuList>
+                    {navMenuItems.map((item, index) => {
+                        if (item.subItems) {
+                            return (
+                                <NavigationMenuItem
+                                    key={index}
+                                    subItems={item.subItems}
+                                >
+                                    <NavigationMenuTrigger>
+                                        <NavigationMenuLink>
+                                            {item.title}
+                                        </NavigationMenuLink>
+                                    </NavigationMenuTrigger>
+
+                                    <NavigationMenuContent>
+                                        <NavigationMenuList>
+                                            {item.subItems.map(
+                                                (subItem, subIndex) => (
+                                                    <NavListItem key={subIndex}>
+                                                        <NavigationMenuLink>
+                                                            {subItem.title}
+                                                        </NavigationMenuLink>
+                                                    </NavListItem>
+                                                )
+                                            )}
+                                        </NavigationMenuList>
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+                            );
+                        }
+
                         return (
-                            <NavigationMenuItem
-                                key={index}
-                                subItems={item.subItems}
-                            >
+                            <NavigationMenuItem key={index}>
                                 <NavigationMenuTrigger>
                                     <NavigationMenuLink>
                                         {item.title}
                                     </NavigationMenuLink>
                                 </NavigationMenuTrigger>
-
-                                <NavigationMenuContent>
-                                    <NavigationMenuList>
-                                        {item.subItems.map(
-                                            (subItem, subIndex) => (
-                                                <NavListItem key={subIndex}>
-                                                    <NavigationMenuLink>
-                                                        {subItem.title}
-                                                    </NavigationMenuLink>
-                                                </NavListItem>
-                                            )
-                                        )}
-                                    </NavigationMenuList>
-                                </NavigationMenuContent>
                             </NavigationMenuItem>
                         );
-                    }
-
-                    return (
-                        <NavigationMenuItem key={index}>
-                            <NavigationMenuTrigger>
-                                <NavigationMenuLink>
-                                    {item.title}
-                                </NavigationMenuLink>
-                            </NavigationMenuTrigger>
-                        </NavigationMenuItem>
-                    );
-                })}
-            </NavigationMenuList>
-        </NavigationMenu>
+                    })}
+                </NavigationMenuList>
+            </NavigationMenu>
+        </div>
     );
 };
 
